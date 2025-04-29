77a102416c990ee6e81dd343b38e4b3e6aa94769
wxss <2198993328@qq.com>
2025-04-29 16:59:40 +0800

    wxss: Update Pull script On Tue Apr 29 16:59:39 2025

--- WorkLog/2025-st/PullScript.sh
+++ WorkLog/2025-st/PullScript.sh
@@ -1,13 +1,11 @@
-getDir() {
+function getPullDir() {
 	for file in `ls $1`
 	do
 		# echo "cd $1"
 		cd $1
-		if test -f $file
-		then
-			echo "file: $file"
-		elif test -d $file
+		if test -d $file
 		then 
+			echo "=================================================="
 			echo "dir: $file"
 			# echo "cd: $file"
 			cd $file
@@ -17,10 +15,82 @@ getDir() {
 			git pull origin master
 
 			echo "RETURN[OK]"
+			echo "=================================================="
 			echo -e "\n"
 		fi
 	done
+
+	return 0
 }
 
-path="D:\Users\ProjectFiles"
-getDir $path
+function getPushApp () {
+	for dir in `ls $1`
+	do
+		cd $1
+		if test -d $dir
+		then
+			curdir=$1$dir
+			cd $curdir
+			echo "=================================================="
+			echo "pwd: `pwd`"
+			for file in `ls $curdir`
+			do
+				if [ "$file" == "$2" ]; then
+					"./"$file
+					echo "RETURN[OK]"
+					echo "=================================================="
+					echo -e "\n"
+				fi
+
+			done
+		fi
+	done
+
+	return 0
+}
+
+
+function chooseOperation () {
+	attemptTimes=5
+	echo "-----------------------------------------------------------------"
+	echo "Please choose the operation: 1 -> getPullDir; 2 -> getPushApp"
+	read -p "Print q/quit/exit to exit: " mode
+	echo "-----------------------------------------------------------------"
+	for i in `seq $attemptTimes`
+	do
+		if [ $i -gt 1 ]; then
+			read -p "Please choose the next operation:" mode
+		fi
+
+		if [ $mode == 1 ]; then
+			getPullDir $homepath
+			echo "--------------------------------------------------"
+		elif [ $mode == 2 ]; then
+			getPushApp $homepath $filename
+			echo "--------------------------------------------------"
+		elif [ "$mode" == "q" -o "$mode" == "quit" -o "$mode" == "exit" ]; then
+			echo -e "\n"
+			echo "--------------------------------------------------"
+			echo "Exit the script."
+
+			break
+
+		else
+			read -p "Please input the correct value: [1] or [2]" mode
+		fi
+	done
+
+	if [ $i -eq $attemptTimes ]; then
+		echo "Attempt Times over $attemptTimes, force exit."
+	fi
+
+	echo "Run Successfully, exit code [0]."
+	echo "--------------------------------------------------"
+
+	return 0
+}
+
+homepath="D:/Users/ProjectFiles/"
+filename="GitPush.exe"
+
+chooseOperation
