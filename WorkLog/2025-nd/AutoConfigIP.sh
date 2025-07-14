1be3df886be163bfd78e9ce60f1202c7dcf6c723
wxss <2198993328@qq.com>
2025-07-14 17:06:32 +0800

    wxss: Update auto config ip script On Mon Jul 14 17:06:32 2025

--- WorkLog/2025-nd/AutoConfigIP.sh
+++ WorkLog/2025-nd/AutoConfigIP.sh
@@ -7,9 +7,7 @@ addr="127.0.0.1"
 mask="255.255.255.0"
 gateway=""
 workMode="a"
-cmdPrefix="Script> "
 command=""
-printPrefixCmd="read -p "$cmdPrefix" command"
 cmdCacheIndex=0
 
 # Print Preview Information
@@ -23,46 +21,18 @@ echo 'Print "help" for more information.'
 # Main Loop
 while true
 do
-	$printPrefixCmd
-	
-	# Catch Arrow Keys
-	# Up	: ^[[A 	Down	: ^[[B
-	# Right	: ^^[C 	Left	: ^[[D
+	read -p "Script> " command
 
-	# read -s -n 1 key
-	# if [[ $key == ${NEO_ESC} ]]; then
-		# read -sn 2 SubListDo
-		# case "$SubListDo" in
-		   # "[A")
-				# cmdCacheIndex=$(($cmdCacheIndex+1 % ${#COMMAND_CACHE[@]}))
-				# echo -n "${COMMAND_CACHE[$cmdCacheIndex]}"
-				# command=${COMMAND_CACHE[$cmdCacheIndex]}
-				# ;;
-		   # "[B")
-				# echo "向下"
-				# ;;
-		   # "[C")
-				# echo "向右"
-				# ;;
-		   # "[D")
-				# echo "向左"
-				# ;;
-		   # *)
-			   # exit
-		# esac
-	# fi
-	
-	
-	
 	# Exit Main Loop
 	if [ "$command" == "q" ]
 	then
-		echo "exit"
+		echo "Exit auto config IP script."
 		break
 	fi
 	
 	# Print Help Information
-	if echo "$command" | grep -qwi "help"
+	# if echo "$command" | grep -qwi "help"
+	if [ "$command" == "help" ]
 	# -q: silence mode; If successful, $? return 0
 	# -w: match string; Ignore case
 	# -i: match by word
@@ -79,10 +49,10 @@ do
 		
 	Lite Mode:
 	    Enter "lite" to entry Lite Mode.
-		Command will use default parameters.
-		Only need to enter IPv4 address.
+	    Command will use default parameters.
+	    Only need to enter Work Mode & IPv4 address.
 	e.g. 
-	cmd Ethernet a 127.0.0.1 255.255.255.0
+	    cmd Ethernet a 127.0.0.1 255.255.255.0
 	EOF
 	command=""
 	continue
@@ -95,8 +65,97 @@ do
 	# Lite Mode
 	if [ "${operationArray[0]}" == "lite" ]
 	then
-		echo "Please Enter IPv4 Address."
-		read -p "LiteMode> " addr
+		echo "Please Enter Work Mode & IPv4 Address."
+		
+		# Lite Mode Main Loop
+		while true
+		do
+			read -p "LiteMode> " liteCmd
+			liteOperationArray=(${liteCmd})
+			liteArrayLen=${#liteOperationArray[@]}
+			if [ $liteArrayLen == 1 ]
+			then
+				case ${liteOperationArray[0]} in
+					"help")
+						cat <<- EOF
+						cmd [name] [work mode] [addr] *[mask] *[gateway] 
+
+						Required parameter:
+							Work mode	: Add[a], Delete[d] or Set[s]
+							IP address	: Ethernet IPv4
+						Optional parameter: 
+							Subnet Mask : [0~255].[0~255].[0~255].[0~255]
+							Gateway     : [0~255].[0~255].[0~255].[0~255]
+							
+						Lite Mode:
+							Enter "lite" to entry Lite Mode.
+							Command will use default parameters.
+							Only need to enter Work Mode & IPv4 address.
+						e.g. 
+							cmd Ethernet a 127.0.0.1 255.255.255.0
+						EOF
+						liteCmd=""
+						continue
+						;;
+					"q")
+						echo "Exit lite mode."
+						break
+						;;
+					*)
+						echo "Please Enter Correct Control Command."
+						echo '"help" or "q"'
+						liteCmd=""
+						continue
+				esac
+			elif [ $liteArrayLen == 2 ]
+			then
+				workMode=${liteOperationArray[0]}
+				addr=${liteOperationArray[1]}
+				
+				liteGeneralCmd="netsh interface ipv4 mode address Ethernet $addr"
+				case $workMode in
+					"a")
+						liteCmd=${liteGeneralCmd/mode/add}
+						workInfo="Added"
+						;;
+					"d")
+						liteCmd=${liteGeneralCmd/mode/delete}
+						workInfo="Deleted"
+						;;
+					*)
+						echo "Please check the work mode format."
+				esac
+			elif [ $liteArrayLen == 0 ]
+			then
+				continue
+			else
+				echo -e "Please Enter Correct Command Format.\n[work mode] [IPv4 address]"
+			fi
+			
+			# Regular Expression Match IP address
+			IPpattern="^([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+		
+			# Check IPv4 Address & Subnet Mask Format
+			if ! [[ "$addr" =~ $IPpattern ]]
+			then
+				echo "Please check IPv4 address format: $addr =~ [0~255].[0~255].[0~255].[0~255]"
+				liteCmd=""
+				continue
+			fi
+		
+			# Execute the Command
+			result=`$liteCmd`
+			if [ "$result" == "" ]
+			then
+				echo "IP Address $workInfo Successfully!"
+				liteCmd=""
+			else
+				echo -e "Ternimal Info: $result\nCommand: $MSCmd"
+			fi
+
+		done
+		liteCmd=""
+		continue
 	elif [ $arrayLen -lt 4 -a $arrayLen -gt 0 ]
 	then
 		echo "Make sure to enter all the required parameter."
@@ -125,12 +184,15 @@ do
 	case $workMode in
 		"a")
 			MSCmd=${MSGeneralCmd/mode/add}$MSCmdSuffix
+			workInfo="Added"
 			;;
 		"d")
 			MSCmd=${MSGeneralCmd/mode/delete}$MSCmdSuffix
+			workInfo="Deleted"
 			;;
 		"s")
 			MSCmd=${MSGeneralCmd/mode/set}$SetMode$MSCmdSuffix
+			workInfo="Set"
 			;;
 		*)
 			echo -e "Please input correct work mode prompt.\nWork mode: Add[a], Delete[d] or Set[s]"
@@ -173,7 +235,7 @@ do
 	result=`$MSCmd`
 	if [ "$result" == "" ]
 	then
-		echo "IP Address Added Successfully!"
+		echo "IP Address $workInfo Successfully!"
 		command=""
 	else
 		echo -e "Ternimal Info: $result\nCommand: $MSCmd"
